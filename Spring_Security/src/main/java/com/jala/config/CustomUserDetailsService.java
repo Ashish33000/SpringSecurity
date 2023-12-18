fb6ffc6c3b469ce8bce8edeb6e17f9e54db1a161
Ashish Kumar <ashishbhagat330@gmail.com>
2023-12-18 13:14:47 +0530

    added

---
+++ Spring_Security/src/main/java/com/jala/config/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.jala.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.jala.model.UserCredential;
+import com.jala.repo.UserCredentialRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserCredentialRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserCredential> credential = repository.findByName(username);
+        return credential.map(CustomUserDetails::new).orElseThrow(() -> new UsernameNotFoundException("user not found with name :" + username));
+    }
+}
